2210bb3391030c34268722b790830f99c513dbef
Khaled Allam <kaaoaa505@gmail.com>
2020-05-25 23:17:00 +0300

    Services Add Transient IAccountService and IAccountRepository

--- MicroRabbit.Infra.IoC/DependancyContainer.cs
+++ MicroRabbit.Infra.IoC/DependancyContainer.cs
@@ -1,6 +1,10 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
+using MircoRabbit.Banking.Application.Interfaces;
+using MircoRabbit.Banking.Application.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,6 +17,12 @@ namespace MicroRabbit.Infra.IoC
         {
             //Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            // Services.
+            services.AddTransient<IAccountService, AccountService>();
+
+            // Data services.
+            services.AddTransient<IAccountRepository, AccountRepository>();
         }
     }
 }
